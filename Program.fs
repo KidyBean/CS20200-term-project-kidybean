1eb440c8546fe69f77129ce7df1eba78daea81b5
_ KidyBean <titon020628@gmail.com>
2026-05-07 00:12:05 +0900

    update

--- Program.fs
+++ Program.fs
@@ -1,2 +1,37 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+type Game1() as this =
+    inherit Game()
+
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spriteBatch: SpriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    do
+        this.Content.RootDirectory <- "Content"
+        this.IsMouseVisible <- true
+
+    override this.Initialize() =
+        base.Initialize()
+
+    override this.LoadContent() =
+        spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+
+    override this.Update(gameTime) =
+        let keyboard = Keyboard.GetState()
+
+        if keyboard.IsKeyDown(Keys.Escape) then
+            this.Exit()
+
+        base.Update(gameTime)
+
+    override this.Draw(gameTime) =
+        this.GraphicsDevice.Clear(Color.CornflowerBlue)
+        base.Draw(gameTime)
+
+[<EntryPoint>]
+let main argv =
+    use game = new Game1()
+    game.Run()
+    0
